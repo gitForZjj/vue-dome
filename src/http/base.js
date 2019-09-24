23276d6606c34a3fffc4d55ad38079115614e14c
zjj <601735095@qq.com>
2019-09-24 18:18:33 +0800

    install

---
+++ src/http/base.js
@@ -0,0 +1,12 @@
+/**
+ * 接口域名的管理
+ */
+const base = {
+  sq: 'http://192.168.1.40:6359/jhzg/api/jhzg/v1.0',
+  bd: 'http://192.168.1.40:6359/jhzg/api/jhzg/v1.0',
+  // sq: 'http://localhost:8080/jhzg/api/jhzg/v1.0',
+  // bd: 'http://localhost:8080/jhzg/api/jhzg/v1.0',
+  // bd: 'http://192.168.1.40:6359/jhzg/api/jhzg/v1.0'
+}
+
+export default base;
